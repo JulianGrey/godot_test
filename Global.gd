2446f191a380671e945430c01f5f7359d8b080c4
Julian Grey <julianrgrey@gmail.com>
2026-02-14 14:23:21 +0000

    Add build number to versioning

--- Global.gd
+++ Global.gd
@@ -2,3 +2,4 @@ extends Node
 
 # The CI pipeline will search for "DEV_BUILD" and replace it.
 var version: String = "DEV_BUILD"
+var build_number: String = "0"
--- versionLabel.gd
+++ versionLabel.gd
@@ -2,4 +2,4 @@ extends Label
 
 func _ready():
 	# Access the singleton we created in Step 1
-	text = "v" + Global.version
+	text = "%s+build.%s" % [Global.version, Global.build_number]
